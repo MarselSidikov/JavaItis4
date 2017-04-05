1b0302c50711fcf0a52eff45109b25cc4cbe3951
Marsel Sidikov <sidikov.marsel@gmail.com>
2017-04-05 19:13:28 +0300

    add all

--- GenericsWithIO/src/ru/itis/zoo/TigerZoo.java
+++ GenericsWithIO/src/ru/itis/zoo/TigerZoo.java
@@ -5,9 +5,21 @@ import ru.itis.cats.Tiger;
 
 public class TigerZoo implements CatZoo<Tiger> {
 
+    private final int MAX_TIGERS_COUNT = 10;
+
+    private Tiger tigers[];
+    private int count = 0;
+
+    public TigerZoo() {
+        this.tigers = new Tiger[MAX_TIGERS_COUNT];
+    }
+
     @Override
     public void add(Tiger animal) {
-
+        if (count < MAX_TIGERS_COUNT) {
+            this.tigers[count] = animal;
+            count++;
+        } else throw new IllegalArgumentException("Нет места");
     }
 
     @Override
