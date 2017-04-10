d21d4fbab5b5c1473764c5448788daeae5997b84
Marsel Sidikov <sidikov.marsel@gmail.com>
2017-04-10 18:37:32 +0300

    add file

---
+++ FilesTask/src/ru/itis/Human.java
@@ -0,0 +1,4 @@
+package ru.itis;
+
+public interface Human {
+}
--- FilesTask/src/ru/itis/HumansFile.java
+++ FilesTask/src/ru/itis/HumansFile.java
@@ -2,17 +2,32 @@ package ru.itis;
 
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 
 // Параметризовать HumansFIle людьми
 // добавить возможность добавления людей в файл
 // добавить метод writeHumans() - запись всех людей в файл
-public class HumansFile implements File {
+public class HumansFile<T extends Human> implements File {
 
     private String name;
+    private ArrayList<T> humans;
 
     public HumansFile(String name) {
         this.name = name;
     }
+
+    public void add(T human) {
+        humans.add(human);
+    }
+
+    public void writeAllToFile() {
+        for (int i = 0; i < humans.size(); i++) {
+            String humanAsString = humans.get(i).toString();
+            write(humanAsString);
+        }
+    }
+
+
     @Override
     public void write(String text) {
         try {
